3a0feb8cfa2617b2b866fc17ace317b5e664ff12
Rodrigo Cabral <rodre.93@gmail.com>
2021-03-23 21:02:18 -0300

    Subida 1

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let miNombre = prompt("Ingrese su nombre");
+let miApellido = prompt("Ingrese su apellido");
+
+console.log(miNombre);
+console.log(miApellido);
+alert("Hola " + miNombre + " " + miApellido);
+
+let miDiaDeNacimiento = prompt("Dia de nacimiento. Ej: 4");
+let miMesDeNacimiento = prompt("Mes de nacimiento. Ej: Enero");
+let miAnioDeNacimiento = prompt("Año de nacimiento. Ej: 1990");
+alert("Naciste el " + miDiaDeNacimiento + " de " + miMesDeNacimiento + " del año " + miAnioDeNacimiento);
+
+
+
